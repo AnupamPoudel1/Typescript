22a80681ebe1ec85b01eff971185f3e2db735c3c
Anupam <anupampoudel78@gmail.com>
2024-05-16 16:21:28 +0545

    end of first lesson of typescript

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
 let username = "Anupam Poudel";
-
 console.log(username);
+
+// the type script accepts this as js accepts this 
+// but the type scripts shows warning as you cant divide number by string
+
+// let a = 18;
+// let b = '6';
+// let c = 3
+// console.log(a / b);
+
+
+let a: number = 18;
+let b: string = '12';
+let c: number = 6;
+
+console.log(a / c);
